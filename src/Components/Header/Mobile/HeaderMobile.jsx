bb12952aef173989c66e191205e059fa524bc64a
Harold Revelo <haroldrevelo@gmail.com>
2021-10-06 09:13:02 -0500

    fix(styles)

--- src/Components/Header/Mobile/HeaderMobile.jsx
+++ src/Components/Header/Mobile/HeaderMobile.jsx
@@ -7,7 +7,7 @@ import FeatherIcon from 'feather-icons-react';
 function HeaderMobile() {
     return (
         <div>
-            <div className={`${style.navbarContainer}`}>
+            <div className={`container-fluid ${style.navbarContainer}`}>
                 <nav className={`row ${style.navbar}`}>
                     <div className={`col-2`}>
                         <button role="button" className={style.sidenavToggle}>
